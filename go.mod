d4bc6733c3fcf3b6bdacf0396cbd9c0ec9f25d70
xiang.wang <xiang.wang@kaiostech.com>
2019-11-18 15:48:17 +0800

    1

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/GingerMoon/m3
+
+go 1.12
+
+require (
+	github.com/GingerMoon/m1 v0.0.2
+	github.com/GingerMoon/m2 v0.0.2
+)
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "github.com/GingerMoon/m1"
+    "github.com/GingerMoon/m2"
+)
+
+func main() {
+    m1.Print() // 2
+    m2.Print() // 2
+}
