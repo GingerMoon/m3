ccd8d2a8109fedaf900a0e2307d3562ff4f1d2f5
xiang.wang <xiang.wang@kaiostech.com>
2020-02-25 16:11:56 +0800

    x

--- main.go
+++ main.go
@@ -1,11 +1,34 @@
 package main
 
-import (
-    "github.com/GingerMoon/m1"
-    "github.com/GingerMoon/m2"
-)
+import "github.com/GingerMoon/m2"
 
+// go.mod:
+//require (
+//    github.com/GingerMoon/m1 v0.0.2
+//    github.com/GingerMoon/m2 v0.0.2
+//)
+//func main() {
+//    m1.Print() // 2
+//    m2.Print() // 2
+//}
+
+// go.mod:
+//require (
+//    github.com/GingerMoon/m1 v0.0.1
+//    github.com/GingerMoon/m2 v0.0.2
+//)
+// go build will change go.mod: github.com/GingerMoon/m1 v0.0.1 -> github.com/GingerMoon/m1 v0.0.2
+//func main() {
+//   m1.Print() // 2
+//   m2.Print() // 2
+//}
+
+// go.mod:
+//require (
+//    github.com/GingerMoon/m1 v0.0.1
+//    github.com/GingerMoon/m2 v0.0.2
+//)
+// go build will change go.mod: github.com/GingerMoon/m1 v0.0.1 -> github.com/GingerMoon/m1 v0.0.2
 func main() {
-    m1.Print() // 2
     m2.Print() // 2
 }
